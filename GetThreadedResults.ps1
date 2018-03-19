bc061e8d80276ed74b3bd8d4d731ee4fad895599
Fabio Freire <fabio@fabio.nyc>
2018-03-19 17:45:09 -0400

    first commit

---
+++ GetThreadedResults.ps1
@@ -0,0 +1,51 @@
+﻿<#
+.SYNOPSIS
+Retrieve data from multiple computers
+
+.DESCRIPTION
+This script will scan all machines in a given list and whatever is specified by $scriptPath
+What sets this script apart is that it uses jobs, so multiple queries run in parallel
+
+.EXAMPLE
+Get-Content computerList.txt | GetThreadedResults -scriptPath .\jobScript.ps1
+#>
+
+Param(
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,Position=0,
+    HelpMessage="Array of computer names.")]
+        [string[]]$targetList,
+
+    [Parameter(Mandatory=$True,Position=1)]
+        [string]$scriptPath
+)
+
+#initialize variables
+$aggregate = @()
+
+#remove any existing jobs
+Get-Job | Remove-Job -Force
+
+Write-Verbose "Launching Jobs..."
+$targetList | % {
+    $boxName = $_
+    #$boxName
+    Start-Job -FilePath $scriptPath -ArgumentList $boxName -Name $boxName | Out-Null
+}
+
+Write-Verbose "Waiting for jobs to complete..."
+get-job | Wait-Job | Out-Null
+
+Write-Verbose "Assembling results..."
+Get-Job | % {
+    $result = Receive-Job  $_
+    
+    $hash = [ordered]@{
+        "Hostname" = $result[0]
+        "RAM (MB)" = $result[1]
+    }
+    
+    $aggregate += (New-Object PSObject -Property $hash)
+}    
+
+$aggregate
+
---
+++ JobScript.ps1
@@ -0,0 +1,12 @@
+﻿$boxName = $args[0]
+
+if ((Test-Connection -ComputerName $boxname -Count 1 -Quiet)) {
+
+    (gwmi -ComputerName $boxName -Class Win32_ComputerSystem).Name
+    [math]::round((gwmi -ComputerName $boxName -Class Win32_ComputerSystem).TotalPhysicalMemory / 1MB, 2)
+
+} else {
+
+    $boxName
+    "Unreachable"
+}
